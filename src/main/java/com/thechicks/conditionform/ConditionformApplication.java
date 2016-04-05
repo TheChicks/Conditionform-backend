d676cf4d7b5d4e0e7cd198f2dafeda86243e5bab
DongHee Kim <opklnm102@gmail.com>
2016-04-05 11:15:32 +0900

    initial commit

---
+++ src/main/java/com/thechicks/conditionform/ConditionformApplication.java
@@ -0,0 +1,12 @@
+package com.thechicks.conditionform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionformApplication.class, args);
+	}
+}
