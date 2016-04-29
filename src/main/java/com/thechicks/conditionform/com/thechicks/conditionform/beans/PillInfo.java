c67f80e4904dc84213622c114458b72bec3dec27
seolheelee <rain00723@gmail.com>
2016-04-29 20:01:08 +0900

    #9 약학정보원 파서 구현중

---
+++ src/main/java/com/thechicks/conditionform/com/thechicks/conditionform/beans/PillInfo.java
@@ -0,0 +1,7 @@
+package com.thechicks.conditionform.com.thechicks.conditionform.beans;
+
+/**
+ * Created by Leeseolhee on 2016. 4. 29..
+ */
+public class PillInfo {
+}
---
+++ src/main/java/com/thechicks/conditionform/com/thechicks/conditionform/utils/PharmaceuticalSourcesCrawler.java
@@ -0,0 +1,7 @@
+package com.thechicks.conditionform.com.thechicks.conditionform.utils;
+
+/**
+ * Created by Leeseolhee on 2016. 4. 28..
+ */
+public class PharmaceuticalSourcesCrawler {
+}
