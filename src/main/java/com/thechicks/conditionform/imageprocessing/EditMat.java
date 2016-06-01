7c3ea611e4797675ab77f79445048f49270bc497
seolheelee <rain00723@gmail.com>
2016-06-01 22:07:12 +0900

    잡음처리 코드 수정

--- src/main/java/com/thechicks/conditionform/imageprocessing/EditMat.java
+++ src/main/java/com/thechicks/conditionform/imageprocessing/EditMat.java
@@ -1,7 +1,8 @@
 package com.thechicks.conditionform.imageprocessing;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Scalar;
 
 import java.awt.image.BufferedImage;
 
@@ -11,29 +12,94 @@ import java.awt.image.BufferedImage;
 public class EditMat {
     Mat mat;
     BufferedImage image;
-    int count;
-    byte blue, green, red;
-    byte min;
 
-    public EditMat(Mat mat, BufferedImage image){
+    public EditMat(Mat mat, BufferedImage image) {
         this.mat = mat;
         this.image = image;
-        count = 0;
-        this.blue = 0;
-        this.green = 0;
-        this.red = 0;
+    }
+
+
+    public Mat edit() {
+
+        removeLetter();
+        return mat;
+    }
+
+
+
+    public void removeLetter() {
+        //Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BayerBG2BGR);
+        //Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2BGR);
+
+//		Core.inRange(mat, new Scalar(0, 0, 0), new Scalar(127, 127, 127), mat);
+
+        // 이미지 가운데 100X100 크기
+        int colsMin = mat.cols()/2-50;
+        int rowsMin = mat.rows()/2-50;
+        int colsMax = colsMin+100;
+        int rowsMax = rowsMin+100;
+        int size = 10000;
+        int pixels[] = new int[mat.cols()*mat.rows()];
+        int sum = 0;
+        int average = 0;
+        int index = 0;
+
+		/*
+		for(int i=rowsMin; i<rowsMax; i++){
+			for(int j=colsMin; j<colsMax; j++){
+				double [] d = mat.get(i, j);
+				pixels[index] = (int)d[0];
+//				System.out.println(pixels[index++]);
+				sum += pixels[index];
+				index++;
+			}
+		}*/
+
+        for(int i=0; i<mat.rows(); i++){
+            for(int j=0; j<mat.cols(); j++){
+                double [] d = mat.get(i, j);
+                pixels[index] = (int)d[0];
+//				System.out.println(pixels[index++]);
+                sum += pixels[index];
+                index++;
+            }
+        }
+        index -= 1;
+        average = sum / index;
+
+        System.out.println(average);
+        average -= 60;
+        Core.inRange(mat, new Scalar(0, 0, 0), new Scalar(average, average, average), mat);
+
+
+			/*
+		if(average > 200){
+			Core.inRange(mat, new Scalar(0, 0, 0), new Scalar(127, 127, 127), mat);
+		}
+		else if(average > 190){
+			Core.inRange(mat, new Scalar(0, 0, 0), new Scalar(110, 110, 110), mat);
+		}
+		else if(average > 170){
+			Core.inRange(mat, new Scalar(0, 0, 0), new Scalar(80, 80, 80), mat);
+		}
+		else{
+
+			Core.inRange(mat, new Scalar(0, 0, 0), new Scalar(146, 110, 110), mat);
+		}
+
+
+		for(int i=0; i<size; i++){
+
+			System.out.print(pixels[i]+" ");
+
+			if(i%100 == 0){
+				System.out.println();
+			}
+		}
+		*/
+//		System.out.println(mat.dump());
 
     }
 
-    public Mat edit(){
-        Mat destination = new Mat(mat.rows(), mat.cols(), mat.type());
-        destination = mat;
-        Imgproc.threshold(mat, destination, 127, 220, Imgproc.THRESH_BINARY);
 
-
-        //Todo: 전처리코드 추가
-
-
-        return destination;
-    }
 }
