612b5c1babcd17dc88c2b13acab6231e78a8b364
seolheelee <rain00723@gmail.com>
2016-06-02 18:59:42 +0900

    타임아웃 설정 추가

--- src/main/java/com/thechicks/conditionform/config/Config.java
+++ src/main/java/com/thechicks/conditionform/config/Config.java
@@ -8,6 +8,9 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
@@ -16,5 +19,17 @@ public class Config {
         ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
         registration.addUrlMappings("/console/*");
         return registration;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(10000);
+        factory.setConnectTimeout(10000);
+        return factory;
     }
 }
